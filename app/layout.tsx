4b05b95d63b44ab41a8b0d24be3cd2118826b8a7
Patrick Gilhooley <pgilhooley95@gmail.com>
2025-04-08 21:28:37 -0400

    Add MFA email verification field to sign-in form

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "./providers";
-import EmailVerificationModal from "@/components/EmailVerificationModal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster />
-          <EmailVerificationModal />
         </Providers>
       </body>
     </html>
